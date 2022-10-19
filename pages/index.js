cd55cbaf2858ece6a48c83a2b7481a82d509f7f8
aryan chauhan <aryanchauhan9758@gmail.com>
2022-10-19 06:11:54 +0530

    50% completed

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
       {/* carousel for tab/mob */}
 
       <div className='mobCarousel'>
-        <Carousel infiniteLoop={true} showIndicators={false} showArrows={false} >
+        <Carousel infiniteLoop={true} showIndicators={false} autoPlay={true} showArrows={false}    >
           <div className="heroImg relative">
 
           </div>
@@ -52,7 +52,7 @@ export default function Home() {
       {/* carousel for laptop  */}
 
       <div className='laptopCarousel'>
-        <Carousel infiniteLoop={true} showIndicators={false}  >
+        <Carousel showIndicators={true}  autoPlay={true} showArrows={false}   infiniteLoop={true}   >
           <div className="heroImg relative">
 
           </div>
@@ -91,7 +91,7 @@ export default function Home() {
         </div>
       </div>
       {/* head mob and tab */}
-      <div className=' absolute headTab -top-5  z-40  right-0  font-nezto flex flex-col space-y-3 px-4 pt-8  justify-end  tracking-widest items-center  '>
+      <div className=' absolute headTab -top-5  z-40  right-0  font-josefin flex flex-col space-y-3 px-4 pt-8  justify-end  tracking-widest items-center  '>
         <Hamburger toggled={isOpen} toggle={setOpen} onClick={modalHandler} />
       </div>
 
@@ -105,15 +105,15 @@ export default function Home() {
         <p className=' h-[16px] font-josefin text-sm  font-normal leading-4 hover:text-[#F0B342] transition duration-300  hover:border-b-2 hover:border-[#F0B342] pb-6'>PROCESS</p>
       </div>
 
-      <div className='absolute herotextTab top-72 px-36 text-white font-nezto  tracking-widest'>
-        <p className='text-7xl herotextTabP1 font-josefin   mb-6'>HEADLINE TEXT</p>
-        <p className='herotextTabP2   font-josefin font-[16px] text-white  leading-6 tracking-[5%] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-        <p className="mt-4  font-josefin font-[16px] text-white  leading-6 tracking-[5%] ">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-        <p className="mt-4 herotextTabP2    font-josefin font-[16px] text-white  leading-6 tracking-[5%]  ">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-        <p className="mt-4 herotextTabP2  font-josefin font-[16px] text-white  leading-6 tracking-[5%]  ">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+      <div className='absolute herotextTab top-52 lg:top-72  px-36 text-white font-josefin  w-full tracking-widest'>
+        <p className=' herotextTabP1 text-2xl font-josefin '>HEADLINE TEXT</p>
+        <p className='herotextTabP2 text-lg  font-josefin font-[16px] text-white  leading-6 tracking-[5%] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+        <p className="mt-4  herotextTabP2 text-lg font-josefin font-[16px] text-white  leading-6 tracking-[5%] ">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+        <p className="mt-4 herotextTabP2 text-lg    font-josefin font-[16px] text-white  leading-6 tracking-[5%]  ">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+        <p className="mt-4 herotextTabP2 text-lg  font-josefin font-[16px] text-white  leading-6 tracking-[5%]  ">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
       </div>
 
-      <div className="absolute px-8  md:px-36 flex dots space-x-8 items-center">
+      {/* <div className="absolute px-8  md:px-36 flex dots space-x-8 items-center">
         <div className="border-orange-300 p-1 border rounded-full ">
           <p className="rounded-full h-3 w-3 bg-orange-300"></p>
         </div>
@@ -121,9 +121,9 @@ export default function Home() {
         <p className="h-3 w-3 rounded-full bg-gray-400"></p>
         <p className="h-3 w-3 rounded-full bg-gray-400"></p>
         <p className="h-3 w-3 rounded-full bg-gray-400"></p>
-      </div>
+      </div> */}
 
-      <div className="flex justify-center  items-center heroText font-nezto font-extrabold tracking-widest ">
+      <div className="flex justify-center  items-center heroText font-josefin font-extrabold tracking-widest ">
         <p className="-mt-10 text-gray-100">MEDIA</p>
       </div>
 
@@ -132,12 +132,12 @@ export default function Home() {
         {/* left div */}
         <div className="lg:pl-12 md:pl-6 py-10 lg:pr-12  card bg-white ">
 
-          <p className="lg:text-4xl md:text-3xl font-bold leading-[100%] w-[284px] h-[40px] not-italic  text-[40px] font-josefin tracking-[0.1em] ">Get In Touch</p>
-          <p className="mt-10  font-bold leading-[100%] w-[61px] h-[16px] not-italic  text-[16px] font-josefin tracking-[0.1em]  ">Mobile</p>
+          <p className="GetinTouch">Get In Touch</p>
+          <p className="mobile">Mobile</p>
 
-          <p className="font-nezto tracking-wider text-sm mt-1">+91 9389725398</p>
-          <p className="mt-6  font-bold leading-['100%'] w-['50px'] h-['16px'] not-italic  text-['16px'] font-josefin tracking-[0.1em] ">Email</p>
-          <p className="font-nezto tracking-wider mt-1 text-sm">groundzero@gmail.com</p>
+          <p className="number">+91 9389725398</p>
+          <p className="email">Email</p>
+          <p className="emailaddress">groundzero@gmail.com</p>
           <button className="text-center bg-black text-white mt-8 px-10 tracking-widest py-3">CONTACT US</button>
 
         </div>
@@ -154,24 +154,24 @@ export default function Home() {
 
             </div>
             <div className='flex justify-center items-center gap-8'>
-              <p className="one "></p>
-              <p className="two "></p>
-              <p className="three"></p>
-              <p className="four "></p>
+              <p className="five "></p>
+              <p className="seven "></p>
+              <p className="eight"></p>
+              <p className="seven"></p>
 
             </div>
             <div className='flex justify-center items-center gap-8'>
-              <p className="one "></p>
-              <p className="two "></p>
-              <p className="three"></p>
-              <p className="four "></p>
+              <p className="twelve "></p>
+              <p className="nine "></p>
+              <p className="elevan"></p>
+              <p className="ten "></p>
 
             </div>
 
 
           </Carousel>
 
-          <Carousel className="newSliding" width={1000} showArrows={true} showIndicators={false} infiniteLoop={true}  >
+          <Carousel className="newSliding" width={1000} showIndicators={false} infiniteLoop={true} autoPlay={true} showArrows={false}  >
             <div className='flex justify-center items-center gap-8'>
               <p className="one "></p>
               <p className="two "></p>
@@ -180,17 +180,17 @@ export default function Home() {
 
             </div>
             <div className='flex justify-center items-center gap-8'>
-              <p className="one "></p>
-              <p className="two "></p>
-              <p className="three"></p>
-              <p className="four "></p>
+              <p className="five "></p>
+              <p className="six "></p>
+              <p className="seven"></p>
+              <p className="eight "></p>
 
             </div>
             <div className='flex justify-center items-center gap-8'>
-              <p className="one "></p>
-              <p className="two "></p>
-              <p className="three"></p>
-              <p className="four "></p>
+              <p className="nine "></p>
+              <p className="ten "></p>
+              <p className="elevan"></p>
+              <p className="twelve"></p>
 
             </div>
 
@@ -213,12 +213,12 @@ export default function Home() {
 
         {/* visible only in mob screen */}
         <div className="lg:pl-12 pl-5 ml-10 xl:ml-0 mt-8 md:mt-0 md:pl-6 py-10 lg:pr-12 otherCard bg-white border  ">
-          <p className="lg:text-4xl text-3xl md:text-3xl font-nezto font-extrabold tracking-widest">Get In Touch</p>
-          <p className="mt-10 font-nezto tracking-wider font-semibold">Mobile</p>
+          <p className="lg:text-4xl text-3xl md:text-3xl font-josefin font-extrabold tracking-widest">Get In Touch</p>
+          <p className="mobile">Mobile</p>
 
-          <p className="font-nezto tracking-wider text-sm mt-1">+91 9389725398</p>
-          <p className="mt-6 font-nezto tracking-wider font-semibold">Email</p>
-          <p className="font-nezto tracking-wider mt-1 text-sm">groundzero@gmail.com</p>
+          <p className="number">+91 9389725398</p>
+          <p className="email">Email</p>
+          <p className='emailaddress'>groundzero@gmail.com</p>
           <button className="text-center bg-black text-white mt-8 px-10 tracking-widest py-3">CONTACT US</button>
         </div>
       </div>
@@ -229,7 +229,7 @@ export default function Home() {
       <div className='mt-16 lg:px-32 md:px-8 '>
         <p className='w-full h-0.5 bg-gray-300'></p>
         <div className='pt-1 pb-4 mt-2 md:mt-0 px-8 md:px-0 md:flex justify-between items-center'>
-          <p className='text-gray-600 text-sm xl:text-normal mb-8 md:mb-0 font-nezto tracking-widest'>	&#169;groundzero | All rights reserved</p>
+          <p className='text-[#787878]  font-Montserrat tracking-[10%] '>	&#169;groundzero | All rights reserved</p>
           <div className='flex space-x-8 text-xl text-gray-700 mt-2'>
             <p className='p-2 rounded-full bg-gray-300'>
               <FaFacebookF />
